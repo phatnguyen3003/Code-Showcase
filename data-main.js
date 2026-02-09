099745cf8e8a6529e46518efbaf82b558c682954
Phát Nguyễn <108886692+phatnguyen3003@users.noreply.github.com>
2026-02-09 10:54:41 +0000

    first upload

---
+++ data-main.js
@@ -0,0 +1,6 @@
+window.MAIN_DATA = {
+    "libraryTitle": "My Coding Showcase",
+    "seriesList": [
+        { "id": "ep_1", "title": "AI Working Together To Build A Simple Game", "desc": "Compare the performance of different AI models in building a simple game." },
+    ]
+};
---
+++ series/ep_1/data-series.js
@@ -0,0 +1,13 @@
+window.SERIES_DATA = {
+    "content": [
+        { "type": "text", "value": "Welcome to my first episode. In this episode, we will try to build a simple game using AI." },
+        { "type": "youtube", "url": "https://youtu.be/bnoPoxBe8mg?si=Q3_FW7bJt_oNzBoc", "title": "Original Video" },
+        { "type": "text", "value": "This is the final result of chat gpt after 10 minutes." },
+        { "type": "file", "path": "chat_gpt.html", "title": "Chat GPT Game" },
+        { "type": "text", "value": "In the next result we got an improve version from Google Gemini." },
+        { "type": "file", "path": "gemini.html", "title": "Google Gemini Game" },
+        { "type": "text", "value": "Lastly is the result from Grok after another 10 minutes." },
+        { "type": "file", "path": "grok.html", "title": "Grok Game" },
+        { "type": "text", "value": "Check out this video about AI game development." }
+    ]
+};
