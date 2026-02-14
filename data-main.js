dd8e50a97f3b16fb9ace1a88f79f012cae9d2137
Phát Nguyễn <108886692+phatnguyen3003@users.noreply.github.com>
2026-02-14 10:59:38 +0000

    update episode 2

--- data-main.js
+++ data-main.js
@@ -2,5 +2,6 @@ window.MAIN_DATA = {
     "libraryTitle": "My Coding Showcase",
     "seriesList": [
         { "id": "ep_1", "title": "AI Working Together To Build A Simple Game", "desc": "Compare the performance of different AI models in building a simple game." },
+        { "id": "ep_2", "title": "AI Working Together To Build A Firework Show", "desc": "Compare the performance of different AI models in building a firework show." },
     ]
 };
---
+++ series/ep_2/data-series.js
@@ -0,0 +1,13 @@
+window.SERIES_DATA = {
+    "content": [
+        { "type": "text", "value": "In this next episode, we will compare two AI models in building a firework show." },
+        { "type": "youtube", "url": "https://www.youtube.com/watch?v=KOQlSgVxwTQ", "title": "Original Video" },
+        { "type": "text", "value": "Some scale is broken and failed to render the page properly, if you want to view the result, please click on the \"open in new tab\" button." },
+        { "type": "text", "value": "This is the final result of chat gpt after 5 chances to fix the issues." },
+        { "type": "file", "path": "chatgpt.html", "title": "Chat GPT Final Result" },
+        { "type": "text", "value": "This is before chat gpt started to fixing the error" },
+        { "type": "file", "path": "chatgpt_before.html", "title": "Chat GPT Before Fixing" },
+        { "type": "text", "value": "And this is the result from Claude code" },
+        { "type": "file", "path": "claude.html", "title": "Claude Final Result" }
+    ]
+};
