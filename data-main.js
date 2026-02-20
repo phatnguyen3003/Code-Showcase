a568453bfa3bbe7156cdcfed277da736428489e6
Phát Nguyễn <108886692+phatnguyen3003@users.noreply.github.com>
2026-02-20 12:19:06 +0700

    upload episode 3
    Added a new series entry for a Reigns game.

--- data-main.js
+++ data-main.js
@@ -3,5 +3,6 @@ window.MAIN_DATA = {
     "seriesList": [
         { "id": "ep_1", "title": "AI Working Together To Build A Simple Game", "desc": "Compare the performance of different AI models in building a simple game." },
         { "id": "ep_2", "title": "AI Working Together To Build A Firework Show", "desc": "Compare the performance of different AI models in building a firework show." },
+        { "id": "ep_3", "title": "AI Working Together To Build A Reigns Game", "desc": "Compare the work of different AI models in building a Reigns game." },
     ]
 };
